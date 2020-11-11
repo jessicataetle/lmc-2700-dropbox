4e7c16b3f16414dc86ccf0c613428e6f9327a45d
jtaetle <jtaetle@gatech.edu>
2020-11-10 21:07:28 -0500

    velocity + ontop of box

--- script.js
+++ script.js
@@ -6,13 +6,11 @@ var canvasWidth = 960;
 var jumping = false;
 var velX = 0;
 var velY = 0;
-var speed = 3;
-var jumpSpeed = 2;
 
 function startGame() {  
     myGameArea.start();
-    outerBox = new component(100, 100, "orange", 0, canvasHeight - 100)
-    innerBox = new component(50, 50, "blue", 25, canvasHeight - 75)
+    outerBox = new outerBox(100, 100, "orange", 0, canvasHeight - 100)
+    innerBox = new innerBox(50, 50, "blue", 25, canvasHeight - 75)
     outerBox.update();
     innerBox.update();
 }
@@ -20,6 +18,7 @@ function startGame() {
 function updateGame() {
     myGameArea.clear();
     document.onkeydown = checkKey.checkKey;
+    document.onkeyup = checkKey.checkKey;
     if (jumping) {
         jump()
     }
@@ -28,65 +27,77 @@ function updateGame() {
    // console.log(outerBox.y)
 }
 
-var checkKey = {
-    checkKey: function(e) {
-    e = e || window.event;
-    if (e.keyCode == '38') {
-        // up arrow
-        //jump()
-        if (!jumping) {
-            jumping = true;
-            if (inBox) {
-                velY = -10;
-            } else {
-                velY = -20;
-            }
-        }
+function updateVel(object) {
+    if (checkKey.up && jumping == false) {
+        velY -= object.velY;
+        jumping = true;
     }
-    else if (e.keyCode == '40') {
-        // down arrow
-    }
-    else if (e.keyCode == '37') {
-       // left arrow
-        if (inBox) {
-            if (outerBox.x > 0) {
-                outerBox.x = outerBox.x - 10;
-                innerBox.x = innerBox.x - 10;
-            }
+
+    if (checkKey.left) {
+        if (object.crashLeft(outerBox)) {
+            object.x = outerBox.x + outerBox.width;
         } else {
-            if (innerBox.x > 0 && !innerBox.crashLeft(outerBox)) {
-                innerBox.x = innerBox.x - 10;
-            }
-        }
-    }
-    else if (e.keyCode == '39') {
-       // right arrow
-        if (inBox) {
-            if (outerBox.x < (960 - outerBox.width)) {
-                outerBox.x = outerBox.x + 10;
-                innerBox.x = innerBox.x + 10;
-            }
-        } else {
-                 if (innerBox.x < (960 - innerBox.width) && !innerBox.crashRight(outerBox)) {
-                    innerBox.x = innerBox.x + 10;
-                }
-        }
-    } else if(e.keyCode == '88') {
-        //X 
-        if(inBox) {
-            if(outerBox.x > 50) {
-                innerBox.x = outerBox.x - 50;
-                innerBox.y = 490;
-                inBox = false;
-            }
-        } else {
-            innerBox.x = outerBox.x + 25;
-            innerBox.y = outerBox.y + 25;
-            inBox = true;
+            velX -= object.velX
+            object.x += velX; 
         }
     }
 
+    if (checkKey.right) {
+        if (object.crashRight(outerBox)) {
+            object.x = outerBox.x - object.width;
+       } else {
+            velX += object.velX
+            object.x += velX; 
+        }
+    }
+    object.y += velY;
+    velX *= 0.9;// friction
+    velY *= 0.9;// friction
+    if (object.x < 0) {
+        velX = 0;
+        object.x = 0;
+    }
+    if (object.x + object.width > canvasWidth) {
+        velX = 0;
+        object.x = canvasWidth - object.width
+    }
 }
+
+
+
+var checkKey = {
+    left: false,
+    right: false,
+    up: false,
+    checkKey: function(e) {
+        e = e || window.event;
+        var key_state = (e.type == "keydown") ? true : false;
+        switch(e.keyCode) {
+            case 37:// left key
+                checkKey.left = key_state;
+                break;
+            case 38:// up key
+                checkKey.up = key_state;
+                break;
+            case 39:// right key
+                checkKey.right = key_state;
+                break;
+        }
+        if (e.keyCode == '88' && e.type == "keydown") {//X 
+            if(inBox) {
+                if(outerBox.x > 50) {
+                    innerBox.x = outerBox.x - 50;
+                    innerBox.y = 490;
+                    velX = 0;
+                    inBox = false;
+                }
+            } else {
+                innerBox.x = outerBox.x + 25;
+                innerBox.y = outerBox.y + 25;
+                inBox = true;
+            }
+        }
+    }
 }
 
 function jump() {
@@ -99,15 +110,22 @@ function jump() {
             jumping = false;
             velY = 0;
             outerBox.y = canvasHeight - 100;
+            innerBox.y = outerBox.y + 25;
         }
     } else {
-        if((innerBox.y + innerBox.height + velY <= 540) || (innerBox.y + innerBox.height) <= outerBox.height) {
-            innerBox.y = velY + innerBox.y;
-            velY++;
-        } else {
-            jumping = false;
+        if(innerBox.crashDown(outerBox)) {
+            innerBox.y = canvasHeight - outerBox.height - innerBox.height
             velY = 0;
-            innerBox.y = canvasHeight - 50
+            jumping = false;
+        } else {
+            if((innerBox.y + innerBox.height + velY <= 540)) {
+                innerBox.y = velY + innerBox.y;
+                velY++;
+            } else {
+                jumping = false;
+                velY = 0;
+                //innerBox.y = canvasHeight - 50
+            }
         }
     }
 }
@@ -126,12 +144,17 @@ var myGameArea = {
     }  
 }
 
-function component(width, height, color, x, y) {  
+function outerBox(width, height, color, x, y) {  
     this.width = width;  
     this.height = height;  
     this.x = x;  
     this.y = y;
+    this.velX = 0.5;
+    this.velY = 10;
     this.update = function() {
+        if (inBox) {
+            updateVel(outerBox);
+        }
         ctx = myGameArea.context;  
         ctx.fillStyle = color;  
         ctx.fillRect(this.x, this.y, this.width, this.height);  
@@ -141,5 +164,37 @@ function component(width, height, color, x, y) {
     }
      this.crashLeft = function(otherobj) {
         return (this.x <= (otherobj.x + otherobj.width)) && (this.x > otherobj.x)
+    }
+    this.crashDown = function(otherobj) {
+        return (this.y + this.height >= otherobj.y) && ((this.x > otherobj.x) || (this.x + this.width < otherobj.x + otherobj.width))
+    }
+}
+
+function innerBox(width, height, color, x, y) {  
+    this.width = width;  
+    this.height = height;  
+    this.x = x;  
+    this.y = y;
+    this.velX = 1;
+    this.velY = 20;
+    this.update = function() {
+        if(inBox) {
+            this.x = outerBox.x + 25;
+            this.y = outerBox.y + 25;
+        } else {
+            updateVel(innerBox)
+        }
+        ctx = myGameArea.context;  
+        ctx.fillStyle = color;  
+        ctx.fillRect(this.x, this.y, this.width, this.height);  
+    }
+    this.crashRight = function(otherobj) {
+       return ((this.x + this.width) >= otherobj.x) && (this.x < otherobj.x) && this.y > otherobj.y
+    }
+     this.crashLeft = function(otherobj) {
+        return (this.x <= (otherobj.x + otherobj.width)) && (this.x > otherobj.x) && this.y > otherobj.y
+    }
+    this.crashDown = function(otherobj) {
+        return (this.y + this.height >= otherobj.y) && ((this.x + this.width > otherobj.x) && (this.x < otherobj.x + otherobj.width))
     }
 }
